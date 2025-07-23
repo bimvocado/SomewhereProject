d5af14ec247c23212df6cc161a8a2f02cf14a738
naeun <nightlock3@naver.com>
2025-07-23 21:50:07 +0900

    기본ui세팅

---
+++ SomewhereProject/Assets/과일실험실/과일Script/Bomb.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private ParticleSystem bombParticle;
+    private void Awake()
+    {
+        bombParticle = GetComponent<ParticleSystem>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            bombParticle.Play();
+            FindAnyObjectByType<GameManager>().Explode();
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SomewhereProject/Assets/과일실험실/과일Script/Fruit.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject whole;
+    public GameObject sliced;
+
+    private Rigidbody fruitRigidbody;
+    private Collider fruitCollider;
+    private ParticleSystem fruitParticle;
+    private void Awake()
+    {
+        //fruitRigidbody = GetComponent<Rigidbody>();
+        fruitCollider = GetComponent<Collider>();
+        fruitParticle = GetComponentInParent<ParticleSystem>();
+        
+        fruitParticle.transform.SetParent(whole.transform);
+    }
+
+    private void Slice(Vector3 direction, Vector3 position, float force)
+    {
+        Object.FindFirstObjectByType<GameManager>().IncreaseScore();
+        whole.SetActive(false);
+        sliced.SetActive(true);
+
+        fruitCollider.enabled = false;
+        fruitParticle.Play();
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody slice in slices)
+        {
+            //slice.angularVelocity = fruitRigidbody.angularVelocity;
+            slice.AddForceAtPosition(direction * force, position, ForceMode.Impulse);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.direction, blade.transform.position, blade.sliceForce);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
