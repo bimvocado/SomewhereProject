16f700cc51854e75727a8f1d59446dc53128228a
bimvocado <163368218+bimvocado@users.noreply.github.com>
2025-07-23 22:05:09 +0900

    햄버거이미지입힘

--- SomewhereProject/Assets/Scripts/Food.cs
+++ SomewhereProject/Assets/Scripts/Food.cs
@@ -1,21 +1,18 @@
 using UnityEngine;
 
-public class Hamboogie : MonoBehaviour
+public class Food : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // 충돌한 오브젝트의 태그가 "Player"일 경우
         if (other.CompareTag("Player"))
         {
             Debug.Log("플레이어와 부딪혔어요!");
-            // 자기 자신(햄버거 오브젝트)을 파괴합니다.
             Destroy(gameObject);
         }
         
         if (other.CompareTag("Ground"))
         {
             Debug.Log("플레이어와 부딪혔어요!");
-            // 자기 자신(햄버거 오브젝트)을 파괴합니다.
             Destroy(gameObject);
         }
     }
---
+++ SomewhereProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Hamboogie;
+    public GameObject Gamza;
+    public GameObject Coke;
+    public GameObject Trash01;
+    public GameObject Trash02;
+    
+    public bool enableSpawn = true; // 생성 여부를 인스펙터에서 제어할 수 있도록 public으로 유지합니다.
+
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnHamboogie", 3f, 3f);
+    }
+
+    void SpawnHamboogie()
+    {
+        if (enableSpawn)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            Instantiate(Hamboogie, new Vector3(randomX, 6.0f, 0f), Quaternion.identity);
+        }
+    }
+    
+    void SpawnGamza()
+    {
+        if (enableSpawn)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            Instantiate(Gamza, new Vector3(randomX, 6.0f, 0f), Quaternion.identity);
+        }
+    }
+    
+    void SpawnCoke()
+    {
+        if (enableSpawn)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            Instantiate(Coke, new Vector3(randomX, 6.0f, 0f), Quaternion.identity);
+        }
+    }
+    
+    void SpawnTrash01()
+    {
+        if (enableSpawn)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            Instantiate(Trash01, new Vector3(randomX, 6.0f, 0f), Quaternion.identity);
+        }
+    }
+    
+    void SpawnTrash02()
+    {
+        if (enableSpawn)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            Instantiate(Trash02, new Vector3(randomX, 6.0f, 0f), Quaternion.identity);
+        }
+    }
+}
