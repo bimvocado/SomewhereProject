13dea4738cbeedaecde18d6cafa65e33cf63a46e
khrr042 <khrr0420@gmail.com>
2025-08-12 03:27:15 +0900

    6 7 오류해결

--- SomewhereProject/Assets/Scripts/AffectionDialogueStarter.cs
+++ SomewhereProject/Assets/Scripts/AffectionDialogueStarter.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using System.Collections.Generic;
-
+using System.Linq;
 public class AffectionDialogueStarter : MonoBehaviour
 {
     [System.Serializable]
@@ -14,36 +14,60 @@ public class AffectionDialogueStarter : MonoBehaviour
     [Header("캐릭터별 스토리 루트")]
     public List<CharacterRoute> characterRoutes;
 
-    private Dictionary<string, AssetReferenceT<DialogueData>> routeDictionary;
-
-    private void Awake()
-    {
-        routeDictionary = new Dictionary<string, AssetReferenceT<DialogueData>>();
-        foreach (var route in characterRoutes)
-        {
-            routeDictionary[route.characterName] = route.dialogueRef;
-        }
-    }
-
     public void StartHighestAffectionRoute()
     {
-        List<string> topCharacters = AffectionManager.Instance.GetCharactersWithHighestAffection();
-        string winner = "";
-
-        if (topCharacters.Count == 1)
+        if (characterRoutes == null || characterRoutes.Count == 0)
         {
-            winner = topCharacters[0];
-        }
-        else if (topCharacters.Count > 1)
-        {
-            int randomIndex = Random.Range(0, topCharacters.Count);
-            winner = topCharacters[randomIndex];
-        }
-
-        if (!string.IsNullOrEmpty(winner) && routeDictionary.TryGetValue(winner, out var routeDialogueRef))
-        {
-            DialogueManager.Instance.StartDialogueFromReference(routeDialogueRef);
             return;
+        }
+
+        if (AffectionManager.Instance == null)
+        {
+            return;
+        }
+
+        List<string> routableCharacters = characterRoutes.Select(route => route.characterName).ToList();
+
+        int maxAffection = -1;
+        List<string> winners = new List<string>();
+
+        foreach (string characterName in routableCharacters)
+        {
+            int currentAffection = AffectionManager.Instance.GetAffection(characterName);
+
+            if (currentAffection > maxAffection)
+            {
+                maxAffection = currentAffection;
+                winners.Clear();
+                winners.Add(characterName);
+            }
+            else if (currentAffection == maxAffection)
+            {
+                winners.Add(characterName);
+            }
+        }
+
+        if (winners.Count == 0)
+        {
+            return;
+        }
+
+        string finalWinner;
+        if (winners.Count == 1)
+        {
+            finalWinner = winners[0];
+        }
+        else
+        {
+            int randomIndex = Random.Range(0, winners.Count);
+            finalWinner = winners[randomIndex];
+        }
+
+        CharacterRoute winnerRoute = characterRoutes.FirstOrDefault(route => route.characterName == finalWinner);
+
+        if (winnerRoute != null && winnerRoute.dialogueRef != null && winnerRoute.dialogueRef.RuntimeKeyIsValid())
+        {
+            DialogueManager.Instance.StartDialogueFromReference(winnerRoute.dialogueRef);
         }
     }
 }
