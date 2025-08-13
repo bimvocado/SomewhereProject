305a0afa5af8f577efb31f9cbbd69e31ee578de9
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2025-08-13 21:32:49 +0900

    game manager

--- SomewhereProject/Assets/CsharpCafe/ChScripts/GameManagerCh.cs
+++ SomewhereProject/Assets/CsharpCafe/ChScripts/GameManagerCh.cs
@@ -1,16 +1,72 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerCh : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static GameManagerCh instance;
+
+    public GameObject backgroundPanel;
+    public GameObject victoryPanel;
+    public GameObject losePanel;
+
+    public int goal;
+    public int moves;
+    public int points;
+
+    public bool isGameEnded;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Initialized(int _moves, int _goal)
+    {
+        moves = _moves;
+        goal = _goal;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ProcessTurn(int _pointsToGain, bool _subtractMoves)
+    {
+        points += _pointsToGain;
+        if (_subtractMoves)
+        {
+            moves--;
+        }
+        if (points >= goal)
+        {
+            isGameEnded = true;
+
+            backgroundPanel.SetActive(true);
+            victoryPanel.SetActive(true);
+            return;
+        }
+        if (moves == 0)
+        {
+            isGameEnded = true;
+            backgroundPanel.SetActive(true);
+            losePanel.SetActive(true);
+            return;
+        }
+    }
+
+    public void WinGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoseGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
